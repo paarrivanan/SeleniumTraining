7d07e2dad892a0b4655cab4a5909a856b9e84082
Rezmi-s <kumarrez@gmail.com>
2021-08-13 14:44:25 +0530

    Added a new Print statement.

--- SeleniumTraining/src/test/java/common/resusableMethods.java
+++ SeleniumTraining/src/test/java/common/resusableMethods.java
@@ -21,6 +21,8 @@ public class resusableMethods {
 
 		System.out.println("the result end here");
 		System.out.println("the result end ");
+		System.out.println("All records fetched from the table ");
+		
 
 	}
 
